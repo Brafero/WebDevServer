d47f6b50111e2d6f5d34931a2a0ee9c774123863
Brafero <139059957+Brafero@users.noreply.github.com>
2024-10-16 23:30:13 -0300

    Arquivos da Semana 03

---
+++ hello2.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/')
+def home():
+    current_time = datetime.utcnow()
+    return render_template('index.html', current_time=current_time)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.route('/rotainexistente')
+def rotainexistente():
+    return render_template('404.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
